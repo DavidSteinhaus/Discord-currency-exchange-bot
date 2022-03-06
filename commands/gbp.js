937ab53aa031dedcf551b728c57dc079f7cf4c47
DavidSteinhaus <71796527+DavidSteinhaus@users.noreply.github.com>
2022-03-06 11:44:28 +0100

    Create gbp.js

---
+++ commands/gbp.js
@@ -0,0 +1,24 @@
+const pupp = require('puppeteer');
+
+module.exports = {
+    name: 'gbp',
+    description:'send image',
+    execute(client, message, args, Discord){
+        (async () => {
+            const browser = await pupp.launch({
+      headless: true,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage'
+      ],
+    });
+            const page = await browser.newPage();
+            await page.goto('https://www.google.com/finance/quote/GBP-TRY?window=1M');
+            let screenshot = await page.screenshot({ path: 'screenshot.png' });
+            await browser.close();
+
+            message.channel.send({files: [screenshot]});
+          })();
+    }
+}
