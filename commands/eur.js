240d737a3ae538d857a77cf7855d9799a05dbe91
DavidSteinhaus <anas.alfakhouri001@stud.fh-dortmund.de>
2022-08-05 22:04:39 +0200

    removed support for all currencies other than usd and euro fixed cookies issue with cheerio

--- commands/eur.js
+++ commands/eur.js
@@ -1,24 +1,37 @@
-const pupp = require('puppeteer');
+const pupp = require("puppeteer");
+const cheerio = require("cheerio");
 
 module.exports = {
-    name: 'eur',
-    description:'send image',
-    execute(client, message, args, Discord){
-        (async () => {
-            const browser = await pupp.launch({
-      headless: true,
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage'
-      ],
-    });
-            const page = await browser.newPage();
-            await page.goto('https://www.google.com/finance/quote/EUR-TRY?window=1M');
-            let screenshot = await page.screenshot({ path: 'screenshot.png' });
-            await browser.close();
+  name: "eur",
+  description: "send image",
+  execute(client, message, args, Discord) {
+    (async () => {
+      const browser = await pupp.launch({
+        headless: true,
+        args: [
+          "--no-sandbox",
+          "--disable-setuid-sandbox",
+          "--disable-dev-shm-usage",
+        ],
+      });
+      const page = await browser.newPage();
+      await page.goto(
+        "https://wise.com/gb/currency-converter/eur-to-try-rate?amount=1"
+      );
+      const pageData = await page.evaluate(() => {
+        return {
+          html: document.documentElement.innerHTML,
+        };
+      });
 
-            message.channel.send({files: [screenshot]});
-          })();
-    }
-}
+      const $ = cheerio.load(pageData.html);
+      const price = $(
+        "#calculator > div.cc-calculator > div.text-xs-center.text-lg-left > h3 > span.text-success"
+      );
+
+      await browser.close();
+
+      message.channel.send("1€ is " + price.text() + "TRY");
+    })();
+  },
+};
--- commands/usd.js
+++ commands/usd.js
@@ -1,24 +1,38 @@
-const pupp = require('puppeteer');
+const pupp = require("puppeteer");
+const cheerio = require("cheerio");
 
 module.exports = {
-    name: 'usd',
-    description:'send image',
-    execute(client, message, args, Discord){
-        (async () => {
-            const browser = await pupp.launch({
-      headless: true,
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage'
-      ],
-    });
-            const page = await browser.newPage();
-            await page.goto('https://www.google.com/finance/quote/USD-TRY?window=1M');
-            let screenshot = await page.screenshot({ path: 'screenshot.png' });
-            await browser.close();
+  name: "usd",
+  description: "send image",
+  execute(client, message, args, Discord) {
+    (async () => {
+      const browser = await pupp.launch({
+        headless: true,
+        args: [
+          "--no-sandbox",
+          "--disable-setuid-sandbox",
+          "--disable-dev-shm-usage",
+        ],
+      });
+      const page = await browser.newPage();
 
-            message.channel.send({files: [screenshot]});
-          })();
-    }
-}
+      await page.goto(
+        "https://wise.com/gb/currency-converter/usd-to-try-rate?amount=1"
+      );
+      const pageData = await page.evaluate(() => {
+        return {
+          html: document.documentElement.innerHTML,
+        };
+      });
+
+      const $ = cheerio.load(pageData.html);
+      const price = $(
+        "#calculator > div.cc-calculator > div.text-xs-center.text-lg-left > h3 > span.text-success"
+      );
+
+      await browser.close();
+
+      message.channel.send("1$ is " + price.text() + "TRY");
+    })();
+  },
+};
