2ee4d1a5f8ee20a82e41849e54e630493b2104af
DavidSteinhaus <anas.alfakhouri001@stud.fh-dortmund.de>
2022-12-23 19:13:06 +0100

    add random cat to Ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,18 @@
+const axios = require("axios");
 module.exports = {
   name: "ping",
   description: "pong",
   execute(client, message, args, Discord) {
     message.channel.send("pong!");
+    (async () => {
+      await axios
+        .get("https://api.thecatapi.com/v1/images/search")
+        .then((res) => {
+          let json = res.data;
+          let url = json[0]?.url;
+          message.channel.send({ files: [url] });
+        })
+        .catch((err) => console.log(err));
+    })();
   },
 };
