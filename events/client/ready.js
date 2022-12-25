d398246d533adef9aeef803d2600f277aedac0f6
DavidSteinhaus <anas.alfakhouri001@stud.fh-dortmund.de>
2022-12-25 13:24:17 +0100

    Imporve start event

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,19 +1,7 @@
-/* module.exports = () =>{
-    console.log("TRYbot is online!");
-} */
-
+const configs = require("../../config/config");
 module.exports = (Discord, client, message) => {
+  if (!configs.myEnmap.has("currencyList")) {
+    configs.myEnmap.set("currencyList", []);
+  }
   console.log("TRYbot is online!");
-  /* const CHANNEL_ID = "931614021138915422";
-  console.log(CHANNEL_ID);
-
-  // Get the Discord channel
-  const channel = client.channels.cache.get(CHANNEL_ID);
-
-  // Schedule a daily message at 3:00 pm
-  setInterval(() => {
-    // Send the message to the Discord channel
-    channel.send("Hello, world!");
-    console.log("hello again");
-  }, 1000); */
 };
