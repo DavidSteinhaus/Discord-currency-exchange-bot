14e61841191ced3a7695a27eabe1078a746663ce
DavidSteinhaus <anas.alfakhouri001@stud.fh-dortmund.de>
2022-12-25 13:24:43 +0100

    Handle errors

--- commands/config.js
+++ commands/config.js
@@ -3,6 +3,9 @@ module.exports = {
   name: "config",
   description: "edit bot configs",
   execute(client, message, args, Discord) {
+    if (!configs.myEnmap.has("currencyList")) {
+      return;
+    }
     let currencyList = configs.myEnmap.get("currencyList");
     if (args.length == 0) {
       if (currencyList.length == 0) {
@@ -24,23 +27,37 @@ module.exports = {
     switch (args[0]) {
       case "add":
         for (let i = 1; i < args.length; i++) {
-          configs.myEnmap.push("currencyList", args[i]);
-          message.channel.send(args[i] + " added successfully!");
+          if (!configs.symbole.includes(args[i].toUpperCase())) {
+            message.channel.send(args[i] + " is not a valid currency symbol.");
+          } else if (currencyList.includes(args[i].toUpperCase())) {
+            message.channel.send(
+              args[i].toUpperCase() + " is already in the list"
+            );
+          } else {
+            configs.myEnmap.push("currencyList", args[i].toUpperCase());
+            message.channel.send(
+              args[i].toUpperCase() + " added successfully!"
+            );
+          }
         }
         break;
       case "remove":
-        if (!currencyList.includes(args[1])) {
+        if (!currencyList.includes(args[1].toUpperCase())) {
           return message.channel.send("this currency is not in the list!");
         }
-        configs.myEnmap.remove("currencyList", args[1]);
-        message.channel.send(args[1] + " removed successfully!");
+        configs.myEnmap.remove("currencyList", args[1].toUpperCase());
+        message.channel.send(args[1].toUpperCase() + " removed successfully!");
         break;
+      case "remove-all":
+        configs.myEnmap.deleteAll();
+        if (!configs.myEnmap.has("currencyList")) {
+          configs.myEnmap.set("currencyList", []);
+        }
+        message.channel.send("All currencies were removed successfully!");
+        break;
+
       default:
         break;
     }
-    //TODO:
-    // ex: !config add for agrs[+1] ....
-    // error handling (only add available currencies)
-    // testing
   },
 };
