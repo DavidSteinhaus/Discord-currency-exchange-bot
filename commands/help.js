2dbdc917c234285a478cfaa7b259dbffd806e0c6
DavidSteinhaus <anas.alfakhouri001@stud.fh-dortmund.de>
2022-12-26 01:35:48 +0100

    Improve help command

--- commands/help.js
+++ commands/help.js
@@ -2,15 +2,6 @@ module.exports = {
   name: "help",
   description: "send user friendly information",
   execute(client, message, args, Discord) {
-    message.channel.send(
-      "to add a new currency please use !config add [currency one] [currency two] ..."
-    );
-    message.channel.send(
-      "to remove a currency please use !config remove [currency]"
-    );
-    message.channel.send(
-      "to remove all currencies please use !config remove-all"
-    );
     const embedMessage = new Discord.MessageEmbed()
       .setColor(0x446b89)
       .setTitle("Available commands: ")
@@ -19,18 +10,18 @@ module.exports = {
           name: "!config add",
           value:
             "adds a new currency. example: !config add [currency one] [currency two] ...",
-          inline: true,
+          inline: false,
         },
         {
           name: "!config remove",
           value:
             "removes a currency that is already in the currency list example: !config remove [currency]",
-          inline: true,
+          inline: false,
         },
         {
           name: "!config remove-all",
           value: "removes all currencies for the configs.",
-          inline: true,
+          inline: false,
         }
       );
     message.channel.send({ embeds: [embedMessage] });
