7b698082739a4f8575959f2dc63b0366ff67b0a1
DavidSteinhaus <anas.alfakhouri001@stud.fh-dortmund.de>
2022-12-26 01:36:06 +0100

    Improve rate command

--- commands/rate.js
+++ commands/rate.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const API = process.env.EXCHANGE_API;
 module.exports = {
   name: "rate",
-  description: "send exchange rate of turkish lira",
+  description: "send exchange rates for a currency",
   execute(client, message, args, Discord) {
     let resultToSend = [];
     (async () => {
@@ -16,20 +16,30 @@ module.exports = {
           fx.base = data.base;
           fx.rates = data.rates;
           let currencyList = configs.myEnmap.get("currencyList");
-          currencyList.map((currency) => {
-            fx.settings = { from: currency, to: "TRY" };
-            let converted = fx.convert(1).toFixed(3);
-            resultToSend.push({ name: currency, value: converted });
-          });
-          const embedMessage = new Discord.MessageEmbed()
-            .setColor(0x446b89)
-            .setTitle("Turkish lira exchange rates:    ")
-            .addFields(
-              resultToSend.map((result) => {
-                return { name: result.name, value: result.value, inline: true };
-              })
+          if (currencyList == 0) {
+            message.channel.send(
+              "there are currently no currencies available. please use !config add to add a new currency."
             );
-          message.channel.send({ embeds: [embedMessage] });
+          } else {
+            currencyList.map((currency) => {
+              fx.settings = { from: currency, to: "TRY" };
+              let converted = fx.convert(1).toFixed(3);
+              resultToSend.push({ name: currency, value: converted });
+            });
+            const embedMessage = new Discord.MessageEmbed()
+              .setColor(0x446b89)
+              .setTitle("Turkish lira exchange rates:    ")
+              .addFields(
+                resultToSend.map((result) => {
+                  return {
+                    name: result.name,
+                    value: result.value,
+                    inline: true,
+                  };
+                })
+              );
+            message.channel.send({ embeds: [embedMessage] });
+          }
         })
         .catch((error) => {
           console.log(error);
