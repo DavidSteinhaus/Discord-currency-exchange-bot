cb891bee12c97ca583ce5045e0425228a3ce03ab
DavidSteinhaus <anas.alfakhouri001@stud.fh-dortmund.de>
2022-12-26 02:34:40 +0100

    Move help command to !config

--- commands/config.js
+++ commands/config.js
@@ -62,6 +62,31 @@ module.exports = {
           `${args[1]} is now the base exchange to currency for the !rate command.`
         );
         break;
+      case "help":
+        const embedMessage = new Discord.MessageEmbed()
+          .setColor(0x446b89)
+          .setTitle("Available commands: ")
+          .addFields(
+            {
+              name: "!config add",
+              value:
+                "adds a new currency. example: !config add [currency one] [currency two] ...",
+              inline: false,
+            },
+            {
+              name: "!config remove",
+              value:
+                "removes a currency that is already in the currency list example: !config remove [currency]",
+              inline: false,
+            },
+            {
+              name: "!config remove-all",
+              value: "removes all currencies for the configs.",
+              inline: false,
+            }
+          );
+        message.channel.send({ embeds: [embedMessage] });
+        break;
       default:
         break;
     }
